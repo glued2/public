d0447341a8c5d6fe2797d60837670592bb545a2c
Nick Riley <95098036+glued2@users.noreply.github.com>
2022-01-25 15:22:34 +0000

    Create Last-Login-Query.ps1
    New Script Created

---
+++ Azure-AD/Last-Login-Query.ps1
@@ -0,0 +1,44 @@
+#AAD Login Times for Everyone (in a specific AD Group)
+#I did start with this guide;  
+#https://thesysadminchannel.com/get-azure-ad-last-login-date-and-sign-in-activity/
+#
+#Install-Module AzureADPreview
+#Import-Module AzureADPreview
+#You'll need to connect to Azure-AD - I tent to just run this interactively, then run the script. 
+#Connect-AzureAD
+#
+
+$ADGroupName = "UK-Sales-Department" 
+$ExportPath = "C:\Users\Nick\$ADGroupName-Export.csv"
+
+#$UsersToCheck = (Get-ADGroupMember -Identity $ADGroupName | Where objectClass -EQ "user" | Get-ADUser | select UserPrincipalName).UserPrincipalName
+Write-Host -f Green "AD Group Query - Complete - Total Found: "$UsersToCheck.count 
+
+$count = 0
+$thiscounter = 0
+$result = @()
+
+foreach ($User in $UsersToCheck) {
+    $UserLower = $User.ToLower()
+     Write-Host -f Cyan "Query about to run for number: $count ($thiscounter of the batch) ($User) "
+    $result += Get-AzureADAuditSignInLogs -Filter "UserPrincipalName eq '$UserLower'" -Top 1 | Select UserPrincipalName,IPAddress,CreatedDateTime
+ 
+    $count++ 
+    $thiscounter ++
+    if ($thiscounter -gt "5"){ 
+            Write-Host -f Red "Sleeping following batch of $thiscounter"
+            $thiscounter = 0 
+            Start-Sleep -s 10
+            }
+}
+
+Write-Host -f Green "Total of $count Records Checked - Writing CSV to $ExportPath " 
+
+echo $result | Export-CSV -Path $ExportPath
+
+
+
+
+
+
+                                                              
